7920d17744ddf8e56eac364e88b2a4b592fae256
rinatnadav <rinat.nadav@fluidoforce.com>
2024-09-22 00:41:18 +0300

    pushing components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from "./pages/HomePage/HomePage";
 import Hamburger from "./components/Hamburger/Hamburger";
 import AboutMe from './pages/AboutMe/AboutMe'; // Adjust the import as needed
