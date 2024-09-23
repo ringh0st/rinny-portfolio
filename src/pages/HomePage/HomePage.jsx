f921f2c33122dfb2dffe00195d5d0ae4536eb168
rinatnadav <rinat.nadav@fluidoforce.com>
2024-09-23 17:22:37 +0300

    pushing components

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -11,35 +11,22 @@ const HomePage = () => {
         navigate('/projects');
     };
 
+    const headerTitle = "In code as in tales, I craft worlds—one pixel, one line at a time.";
     return (
         <container className={styles.home}>
             <header className={styles.portfolioHeader}>
                 <h1 className={styles.headerTitle}>
-                    "In code as in tales, I craft worlds—one pixel, one line at a time."
+                    {headerTitle}
                 </h1>
                 <p className={styles.headerSubtitle}>
                     — Rinat Nadav, weaver of web, words, and wonder.
                 </p>
             </header>
-
-            {/*<section className={styles.homeHero}>*/}
-            {/*    <h1>In code as in tales, I craft worlds—one pixel, one line at a time.</h1>*/}
-            {/*    /!*<p>I'm a Frontend Developer.</p>*!/*/}
-            {/*    /!*<button className={styles.homeCta}>Explore my work</button>*!/*/}
-            {/*    <p></p>*/}
-            {/*</section>*/}
             <main className={styles.home}>
                 <section>
                     <Button onClick={handlePrimaryClick} variant="primary" text="Explore My Work"/>
                 </section>
             </main>
-
-            {/*<section className={styles.homeProjects}>*/}
-            {/*    <h2>Recent Projects</h2>*/}
-            {/*</section>*/}
-
-            {/*<section className={styles.homeAbout}>*/}
-
         </container>
     );
 };
